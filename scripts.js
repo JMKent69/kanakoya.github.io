0390d3dfbf12144048fcd8f2ed2f7021e075c04e
JMKent69 <jokent@jplus.co.uk>
2024-05-26 15:05:29 +0900

    Major menu and formatting changes

--- scripts.js
+++ scripts.js
@@ -40,17 +40,23 @@ function setLanguage(lang) {
     }
 }
 
-// Function to load the menu bar
-function loadMenu() {
-    fetch('menu.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('menu-placeholder').innerHTML = data;
-            setLanguage('en'); // Initialize the language
-        });
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 // Initialize the page
 window.onload = function() {
-    loadMenu();
+    setLanguage('en');
+    showSlides();
 };
